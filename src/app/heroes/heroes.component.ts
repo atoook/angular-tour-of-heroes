0f9883b7c47fa704db0228db6a307ac1b8b69d1e
Kota Kawamoto <atok.reverse.1040@gmail.com>
2024-11-17 18:10:00 +0900

    add a new hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -29,6 +29,15 @@ export class HeroesComponent {
       .subscribe(heroes => this.heroes = heroes);
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
   // ** method below is no longer used
   // onSelect(hero: Hero): void{
   //   this.selectedHero = hero;
