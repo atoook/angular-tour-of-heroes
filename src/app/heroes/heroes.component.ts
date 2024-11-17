ac0d99eb3c61648604d2b0674fe5b7a3caa5fb42
Kota Kawamoto <atok.reverse.1040@gmail.com>
2024-11-17 11:59:57 +0900

    import FormsModule in AppModule to use 'ngModel' directive

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import  { Hero } from '../hero';
 
 @Component({
-  selector: 'app-heroes',
+  selector: 'app-heroes', //used as "<app-heroes></app-heroes>"" in  app.component.html
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
