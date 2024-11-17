1d92afd2a2d9a139ec69b4154cc81d3b34706bff
Kota Kawamoto <atok.reverse.1040@gmail.com>
2024-11-17 15:10:00 +0900

    Refactor HeroService to return Observable from getHeroes()

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import  { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes', //used as "<app-heroes></app-heroes>"" in  app.component.html
@@ -8,8 +9,23 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes :Hero[] = [];
   selectedHero?: Hero;
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    // **Original
+    // this.heroes = this.heroService.getHeroes();
+    
+    // **Subscribe
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
