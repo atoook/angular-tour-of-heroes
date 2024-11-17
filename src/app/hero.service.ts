daa86f930e7c4b0eec87de416e7166c296897dac
Kota Kawamoto <atok.reverse.1040@gmail.com>
2024-11-17 17:48:02 +0900

    get heroes with HttpClient

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,16 +5,24 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  /**
+   * Define the heroesUrl of the form :base/:collectionName
+   * with the address of the heroes resource on the server. 
+   * Here base is the resource to which requests are made, 
+   * and collectionName is the heroes data object in the in-memory-data-service.ts.
+   */
+  private heroesUrl = 'api/heroes';  // URL to web api
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient, private messageService: MessageService) { }
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetch heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
@@ -23,5 +31,10 @@ export class HeroService {
     const hero = HEROES.find(h => h.id === id)!;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
